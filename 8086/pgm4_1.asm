c4b1a896b762c9778d97c3ba4eda8d289a94b295
r-kill <rowan.kill@live.com>
2019-01-15 18:43:08 -0600

    Update pgm4_1.asm

--- 8086/pgm4_1.asm
+++ 8086/pgm4_1.asm
@@ -14,14 +14,14 @@ INT 21H       ; display char
 
 ; input a char
 MOV AH,1	  ; read character function
-INT 21H       ; get input character in AL
+INT 21H     ; get input character in AL
 MOV BL,AL	  ; save character in BL
 
 ; go to newline
 MOV AH,2	  ; display character function
-MOV DL,0DH    ; 0DH is carriage return code
-INT 21H       ; execute carriage return
-MOV DL,0AH    ; 0AH is line feed code
+MOV DL,0DH      ; 0DH is carriage return code
+INT 21H         ; execute carriage return
+MOV DL,0AH      ; 0AH is line feed code
 INT 21H		  ; execute line feed
 
 ; display character
