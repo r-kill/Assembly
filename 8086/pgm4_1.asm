a10e25128afd3f38e8a65231cb43c9bb012d979e
r-kill <rowan.kill@live.com>
2019-01-15 18:41:44 -0600

    Get input and display it on the terminal.

--- 8086/pgm4_1.asm
+++ 8086/pgm4_1.asm
@@ -5,29 +5,29 @@
 
 #fasm#		  ; use intel style
 
-ORG	100H	  ; define starting point
+ORG 100H	  ; define starting point
 
 ; display prompt
-MOV	AH,2	  ; display character function
-MOV	DL,'?'	  ; character is '?'
-INT	21H       ; display char
+MOV AH,2	  ; display character function
+MOV DL,'?'	  ; character is '?'
+INT 21H       ; display char
 
 ; input a char
-MOV	AH,1	  ; read character function
-INT	21H       ; get input character in AL
-MOV	BL,AL	  ; save character in BL
+MOV AH,1	  ; read character function
+INT 21H       ; get input character in AL
+MOV BL,AL	  ; save character in BL
 
 ; go to newline
-MOV	AH,2	  ; display character function
-MOV	DL,0DH    ; 0DH is carriage return code
-INT	21H       ; execute carriage return
-MOV	DL,0AH    ; 0AH is line feed code
+MOV AH,2	  ; display character function
+MOV DL,0DH    ; 0DH is carriage return code
+INT 21H       ; execute carriage return
+MOV DL,0AH    ; 0AH is line feed code
 INT 21H		  ; execute line feed
 
 ; display character
-MOV	DL,BL	  ; move char from BL (above) to DL
+MOV DL,BL	  ; move char from BL (above) to DL
 INT 21H		  ; display char that's now in DL
 
 ; return to DOS
 MOV AH,4CH	  ; DOS exit function code
-INT	21H       ; exit to DOS
+INT 21H       ; exit to DOS
